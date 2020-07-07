288fcee04673609e3f7f00d96fe630b5af9f53af
PhiCecHey <65090580+PhiCecHey@users.noreply.github.com>
2020-07-07 14:38:28 +0200

    Update a6

--- source/a_5.cpp
+++ source/a_5.cpp
@@ -25,6 +25,19 @@ TEST_CASE("sort circles 4.5", "[sort 4.5]") {
 	REQUIRE(std::is_sorted(sorted_circles.begin(), sorted_circles.end()));
 }
 
+// 4.6
+TEST_CASE("sort circles 4.6", "[sort 4.6]") {
+	std::list<Kreis> sorted_circles;
+	sorted_circles.push_back({ 1 });
+	sorted_circles.push_back({ 3 });
+	sorted_circles.push_back({ 5 });
+	sorted_circles.push_back({ 2 });
+	sorted_circles.push_back({ 2 });
+	auto sorting_lambda = [](Kreis const& a, Kreis const& b) {return a.radius() < b.radius(); };
+	sorted_circles.sort(sorting_lambda);
+	REQUIRE(std::is_sorted(sorted_circles.begin(), sorted_circles.end()));
+}
+
 int main(){
 return 0;
 }
